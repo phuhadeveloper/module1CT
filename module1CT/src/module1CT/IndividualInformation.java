128d655401ba9dc67040d69a362e65c7e14c1a04
phuth <phuth@LAPTOP-69H0B4VV>
2023-09-17 11:41:31 -0400

    Create IndividualInformation class.

---
+++ module1CT/src/module1CT/IndividualInformation.java
@@ -0,0 +1,24 @@
+package module1CT;
+
+import java.util.Scanner;
+
+public class IndividualInformation {
+
+	public static void main(String[] args) {
+		//Create 5 variables and store First Name,
+		//Last Name, Street Address, City, and Zip Code
+		String firstName = "John";
+		String lastName = "Doe";
+		String streetAddress = "123 Culver St";
+		String city = "Los Angeles";
+		String zipCode = "90210";
+		
+		//Print out the individual's information
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Street Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zip Code: " + zipCode);
+	}
+
+}
